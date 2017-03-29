56f3e1683c39080106d8aa171c6e2699b841a2e6
moody <moody@gmail.com>
2017-03-29 16:35:45 +0200

    Solved problem with hello_world and created publisher.

--- src/hello_world.cpp
+++ src/hello_world.cpp
@@ -1,10 +1,13 @@
 #include <ros/ros.h>
 #include <iostream>
 int main(int argc, char** argv) {
-  //ros::init(argc, argv, "hello_world");
-  //ros::NodeHandle nh;
+  ros::init(argc, argv, "hello_world");
+  ros::NodeHandle nh;
+
   std::cout <<"Hello, world!" << std::endl;
-  //ros::spin();
+
+  ros::spin();
+
   return 0;
 
 }
