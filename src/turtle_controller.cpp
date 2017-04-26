4e3ee7552ea3c8d2dad001e7bd1b67f5c222504d
moody <moody@gmail.com>
2017-04-26 15:57:17 +0200

    added few changes related to turtle

--- src/turtle_controller.cpp
+++ src/turtle_controller.cpp
@@ -2,40 +2,61 @@
 #include <turtlesim/Pose.h>
 #include <geometry_msgs/Twist.h>
 
+double x_d, y_d, theta_d;
 
-double k_Pf, k_Po;
-double x_d, y_d;
-double forward_control = 0.0;
-double orientation_control = 0.0;
+double forward_control=0.0;
+double orientation_control=0.0;
+double k_pf , k_po;
 
-void poseCallback(const turtlesim::Pose::ConstPtr& pose_msg){
-  double x_error = x_d - pose_msg ->x;
-  double y_error = y_d - pose_msg ->y;
 
-  forward_control =  k_Pf * x_error;
-  orientation_control =  k_Po * y_error;
+
+void poseCallback(const turtlesim::Pose::ConstPtr& pose_msg)
+{
+
+ double x_error = x_d - pose_msg->x;
+ double y_error = y_d - pose_msg->y;
+ theta_d= atan2(y_error,x_error);
+ double theta_error = theta_d - pose_msg->theta;
+ //to do:
+ // compute orientation_control= ....
+ // Hint : use atan2(y_error, x_error)
+
+ forward_control = 1.0;
+ orientation_control = k_po * theta_error;
+if  (pow(x_error, 2.0) + pow (y_error, 2.0) < 0.01) {
+  forward_control =0.0;
+  orientation_control=0.0;
 }
 
-int main(int argc, char** argv){
+}
+
+int main(int argc, char** argv) {
   ros::init(argc, argv, "turtle_controller");
   ros::NodeHandle nh;
+
   ros::Publisher control_pub = nh.advertise<geometry_msgs::Twist>("/turtle1/cmd_vel", 10);
   ros::Subscriber pose_sub = nh.subscribe("/turtle1/pose", 10, &poseCallback);
 
-  nh.param ("goal_x", x_d, 7.0);
-  nh.param ("goal_y", y_d, 7.0);
-  nh.param ("surge_gain", k_Pf, 1.0);
-  nh.param ("orientation_gain", k_Po, 1.0);
-
+  nh.param("goal_x", x_d, 7.0);
+  nh.param("goal_y", y_d, 7.0);
+  nh.param("Surge_gain", k_pf, 1.0);
+  nh.param("Orientiation_gain", k_po, 10.0);
 
   ros::Rate rate(100.0);
-  while (ros::ok()) {
+
+  while (ros::ok())
+  {
     ros::spinOnce();
+
     geometry_msgs::Twist controls;
     controls.linear.x = forward_control;
-    controls.angular.z = 0.0;
+    controls.angular.z = orientation_control;
+
+
     control_pub.publish(controls);
+
     rate.sleep();
- }
+  }
+
   return 0;
 }
